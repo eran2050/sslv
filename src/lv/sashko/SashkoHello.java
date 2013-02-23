2ad1505c6456f1af05e3e616d9868ffa938a809f
suxlv2 <suxlv2@gmail.com>
2013-02-23 17:42:53 +0200

    Volkov 1

--- src/lv/sashko/SashkoHello.java
+++ src/lv/sashko/SashkoHello.java
@@ -13,7 +13,7 @@ public class SashkoHello {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hayduk, Kinder!");
+		System.out.println("Hayduk, Mein Kinder!");
 
 	}
 
